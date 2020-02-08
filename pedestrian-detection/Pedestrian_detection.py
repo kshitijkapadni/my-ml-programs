d1012228d99633f4959ded36436e3072b9a27562
Kshitij Kapadni <46843070+ksk-28-11@users.noreply.github.com>
2020-02-08 09:08:26 +0530

    Update Pedestrian_detection.py

--- pedestrian-detection/Pedestrian_detection.py
+++ pedestrian-detection/Pedestrian_detection.py
@@ -1,18 +1,11 @@
 #importing necessay packages for pedestrian detection 
 
 from __future__ import print_function
-#from imutils.object_detection import non_max_suppression
 from imutils import paths
 import imutils
 import numpy as np
-#import argparse
 
 import cv2
-
-## construct the argument parse and parse the arguments
-#ap = argparse.ArgumentParser()
-#ap.add_argument("-i", "--images", required=True, default="D:\kskML\image processing\Pedestrian detection\Pedestrian detection\images")
-#args = vars(ap.parse_args())
  
 # initialize the HOG descriptor
 hog = cv2.HOGDescriptor()
@@ -23,7 +16,6 @@ for imagePath in paths.list_images("D:\kskML\image processing\Pedestrian detecti
     print(imagePath)
     image = cv2.imread(imagePath)
     img = image.copy()
-    #image = cv2.resize(image,(500,780))
     orig = imutils.resize(image, width=min(400, image.shape[1]))
    
 
@@ -36,6 +28,6 @@ for imagePath in paths.list_images("D:\kskML\image processing\Pedestrian detecti
     else:
         for (x, y, w, h) in rects:
             cv2.rectangle(orig, (x, y), (x + w, y + h), (0, 0, 255), 2)
-        cv2.imshow("After NMS", orig)
+        cv2.imshow("peds", orig)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
